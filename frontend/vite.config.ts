1eebcbff2a41a3a7234dc2facdd4d568a1adce08
Rajat Sharma <82762243+silver-times@users.noreply.github.com>
2023-11-11 08:56:23 +0100

    update vite.config and node version

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,11 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   server: {
+    watch: {
+      usePolling: true,
+    },
     host: true,
+    strictPort: true,
     port: 8080,
   },
 });
